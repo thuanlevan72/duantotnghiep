332af82a6d1d9213a4b1609d18b5f731f08af20a
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2023-06-13 22:37:09 +0700

    update code 2 2023/06/13

--- api/FOLYFOOD/Dto/InfoDto/InfoRequets.cs
+++ api/FOLYFOOD/Dto/InfoDto/InfoRequets.cs
@@ -2,11 +2,12 @@
 {
     public class InfoRequets
     {
-        public int InfoId { get; set; }
+        public int? InfoId { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Title { get; set; }
+        public bool? IsShow { get; set; }
     }
 }
